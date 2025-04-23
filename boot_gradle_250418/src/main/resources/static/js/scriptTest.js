8d07f496c676ab60bf46871f00dafc7cbc164778
LEE <69142830+LeeStrike90@users.noreply.github.com>
2025-04-23 09:17:32 +0900

    4.23

---
+++ boot_gradle_250418/src/main/resources/static/js/scriptTest.js
@@ -0,0 +1,4 @@
+function test() 
+{
+	alert("spring boot 테스트 입니다.");
+}
---
+++ boot_gradle_test/src/main/java/com/boot/test/BootGradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleTestApplication.class, args);
+	}
+
+}
